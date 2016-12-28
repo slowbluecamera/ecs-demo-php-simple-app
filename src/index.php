629279adee9e232317178f99e2dae681ad1b8904
David Benjamin <david@slowbluecamera.com>
2016-12-28 13:49:25 +1100

    minor test tweak

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations, you're the puppet.</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
@@ -24,7 +24,6 @@
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                 ?>
-
             </div>
         </div>
 
